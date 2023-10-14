501ee143ccd82746fc30652eb0be04c3855ece13
Phong Bui <boushphong@gmail.com>
2023-10-14 13:53:22 +0700

    Add test Dag under parsing process dag

---
+++ airflow/dags/dag_file_under_parsing_process_3.py
@@ -0,0 +1,12 @@
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+from datetime import timedelta
+from pendulum import today
+
+dag = DAG(
+    "dag_file_under_parsing_process_no3",
+    start_date=today() - timedelta(days=5),
+    schedule="0 0 * * *",
+    catchup=True,
+)
+task = EmptyOperator(task_id="empty_task", dag=dag)
